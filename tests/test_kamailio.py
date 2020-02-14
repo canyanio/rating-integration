7f0a2c0bda0107b8eae1e66397307e4dab69d072
Fabio Tranchitella <fabio@tranchitella.eu>
2020-02-14 11:26:16 +0100

    Switch testing to canyan/canyantester
    Signed-off-by: Fabio Tranchitella <fabio@tranchitella.eu>

--- tests/test_kamailio.py
+++ tests/test_kamailio.py
@@ -1,7 +1,7 @@
 import os
 
 from click.testing import CliRunner
-from wazotester import wazotester
+from canyantester import canyantester
 
 
 TARGET = os.environ.get('TARGET', 'kamailio:5060')
@@ -11,40 +11,40 @@ API_URL = os.environ.get('API_URL', 'http://rating-api:8000')
 def test_kamailio_authorized():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, 'scenarios', 'test_kamailio_authorized.yaml')
-    result = CliRunner().invoke(wazotester, ['-a', API_URL, '-t', TARGET, scenario_file])
+    result = CliRunner().invoke(canyantester, ['-a', API_URL, '-t', TARGET, scenario_file])
     assert result.exit_code == 0
 
 
 def test_kamailio_unauthorized_balance_insufficient():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, 'scenarios', 'test_kamailio_unauthorized_balance_insufficient.yaml')
-    result = CliRunner().invoke(wazotester, ['-a', API_URL, '-t', TARGET, scenario_file])
+    result = CliRunner().invoke(canyantester, ['-a', API_URL, '-t', TARGET, scenario_file])
     assert result.exit_code == 0
 
 
 def test_kamailio_unauthorized_not_active():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, 'scenarios', 'test_kamailio_unauthorized_not_active.yaml')
-    result = CliRunner().invoke(wazotester, ['-a', API_URL, '-t', TARGET, scenario_file])
+    result = CliRunner().invoke(canyantester, ['-a', API_URL, '-t', TARGET, scenario_file])
     assert result.exit_code == 0
 
 
 def test_kamailio_unauthorized_not_found():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, 'scenarios', 'test_kamailio_unauthorized_not_found.yaml')
-    result = CliRunner().invoke(wazotester, ['-a', API_URL, '-t', TARGET, scenario_file])
+    result = CliRunner().invoke(canyantester, ['-a', API_URL, '-t', TARGET, scenario_file])
     assert result.exit_code == 0
 
 
 def test_kamailio_unauthorized_too_many_transactions():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, 'scenarios', 'test_kamailio_unauthorized_too_many_transactions.yaml')
-    result = CliRunner().invoke(wazotester, ['-a', API_URL, '-t', TARGET, scenario_file])
+    result = CliRunner().invoke(canyantester, ['-a', API_URL, '-t', TARGET, scenario_file])
     assert result.exit_code == 0
 
 
 def test_kamailio_unauthorized_unreachable():
     base_dir = os.path.dirname(__file__)
     scenario_file = os.path.join(base_dir, 'scenarios', 'test_kamailio_unauthorized_unreachable.yaml')
-    result = CliRunner().invoke(wazotester, ['-a', API_URL, '-t', TARGET, scenario_file])
+    result = CliRunner().invoke(canyantester, ['-a', API_URL, '-t', TARGET, scenario_file])
     assert result.exit_code == 0
